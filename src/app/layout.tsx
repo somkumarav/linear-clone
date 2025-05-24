e99c3a9cc042b8a5f607d78bee056a7f30206314
somkumarav <somkumarav@gmail.com>
2025-05-24 15:11:12 +0530

    removed unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Container } from "../components/container";
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 
